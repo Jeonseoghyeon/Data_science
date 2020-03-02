f4b39589d0f677dc1e94ad51932f9991ddf69f9a
Jeonseoghyeon <jsh28592@gmail.com>
2020-03-03 00:55:50 +0900

    0302 DS study

--- asd.py
+++ asd.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
+df = pd.read_csv('data/enrolment_1.csv')
+
 # 코드를 작성하세요.
-df = pd.read_csv('data/popular_baby_names.csv')
+df['status'] = 'allowed'
+condition1 = (df['year'] == 1) 
+df.loc[condition1,'status'] == 'not allowed'
 # 정답 출력
 print(df)
